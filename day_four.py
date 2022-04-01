3300435cce0d487782dbe932cbd250fd95130b4c
Kiril Peshev <96590371+peshevk@users.noreply.github.com>
2022-04-01 21:20:07 +0300

    Add files via upload

---
+++ day_four.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+for x in range(1,11):
+    print(f"{n} x {x} = {n*x}")
+
+#hello github
